61a76685f5fc58736622494c90e06133d24284d9
econsergio@gmail.com <econsergio@gmail.com>
2019-06-25 01:23:41 +0100

    cached thread pool

--- raw-java-socket/src/main/java/com/sergiomartinrubio/rawjavasocket/RawSocket.java
+++ raw-java-socket/src/main/java/com/sergiomartinrubio/rawjavasocket/RawSocket.java
@@ -6,29 +6,38 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class RawSocket {
 
     private static final int PORT_NUMBER = 8080;
 
     public static void main(String[] args) throws IOException {
-
         ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
+
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+
 
         while (true) {
             Socket server = serverSocket.accept();
 
-            BufferedReader in = new BufferedReader(new InputStreamReader(server.getInputStream()));
-            OutputStream out = server.getOutputStream();
-            in.lines().forEach(line -> {
+            threadPool.execute(() -> {
                 try {
-                    out.write(("Echo: " + line.toUpperCase() + "\n").getBytes());
+                    BufferedReader in = new BufferedReader(new InputStreamReader(server.getInputStream()));
+                    OutputStream out = server.getOutputStream();
+                    in.lines().forEach(line -> {
+                        try {
+                            out.write(("Echo: " + line.toUpperCase() + "\n").getBytes());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+                    server.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-
             });
-            server.close();
         }
     }
 }
