2586be573b5efbe6b77533c5afbd17c87bcf8c1e
Sergio Martin Rubio <smartinrubio@hotels.com>
2019-06-28 16:10:28 +0100

    refactor

--- java-socket-channel/src/main/java/com/sergiomartinrubio/javasocketchannel/SocketChannelWithSelectorServer.java
+++ java-socket-channel/src/main/java/com/sergiomartinrubio/javasocketchannel/SocketChannelWithSelectorServer.java
@@ -2,8 +2,6 @@ package com.sergiomartinrubio.javasocketchannel;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.net.Socket;
-import java.net.SocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
@@ -48,15 +46,15 @@ public class SocketChannelWithSelectorServer {
                     socketChannel.register(selector, SelectionKey.OP_READ);
                 } else if (selectionKey.isReadable()) {
                     System.out.println("Reading...");
-                    SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
-                    ByteBuffer pendingData = dataMap.get(socketChannel); // find socket channel to retrieve pending data if any
+                    var socketChannel = (SocketChannel) selectionKey.channel();
+                    var pendingData = dataMap.get(socketChannel); // find socket channel to retrieve pending data if any
                     int read = socketChannel.read(pendingData);
 
                     if (read == -1) {
                         dataMap.remove(socketChannel);
-                        Socket socket = socketChannel.socket();
-                        SocketAddress remoteAddr = socket.getRemoteSocketAddress();
-                        System.out.println("Connection closed by client: " + remoteAddr);
+                        var socket = socketChannel.socket();
+                        var remoteSocketAddress = socket.getRemoteSocketAddress();
+                        System.out.println("Connection closed by client: " + remoteSocketAddress);
                         socketChannel.close();
                         selectionKey.cancel();
                     }
@@ -66,8 +64,8 @@ public class SocketChannelWithSelectorServer {
                     socketChannel.register(selector, SelectionKey.OP_WRITE); // set mode to WRITE to send data
                 } else if (selectionKey.isWritable()) {
                     System.out.println("Writing...");
-                    SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
-                    ByteBuffer pendingData = dataMap.get(socketChannel);
+                    var socketChannel = (SocketChannel) selectionKey.channel();
+                    var pendingData = dataMap.get(socketChannel);
                     while (pendingData.hasRemaining()) {
                         socketChannel.write(pendingData); // sends all data at once
                     }
